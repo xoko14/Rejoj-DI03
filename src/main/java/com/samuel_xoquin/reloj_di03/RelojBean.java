61a55bd82015df6bbfeeec46e034a725eb52fd8b
xoko14 <xoquinperezb@gmail.com>
2021-11-04 19:33:10 +0100

    fin clase 04/11/2021

--- src/main/java/com/samuel_xoquin/reloj_di03/RelojBean.java
+++ src/main/java/com/samuel_xoquin/reloj_di03/RelojBean.java
@@ -1,10 +1,13 @@
 package com.samuel_xoquin.reloj_di03;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.Serializable;
-import javax.swing.JFrame;
+import java.time.LocalTime;
+import javax.swing.JLabel;
 import javax.swing.Timer;
 
-public class RelojBean extends JFrame implements Serializable {
+public class RelojBean extends JLabel implements Serializable, ActionListener {
     public static final boolean FORMATO_24 = false;
     public static final boolean FORMATO_12 = true;
     
@@ -54,11 +57,25 @@ public class RelojBean extends JFrame implements Serializable {
         this.mensaje = mensaje;
     }
     
-    private Timer tHora;
-    private Timer tMin;
+    private Timer horaFinder;
 
     public RelojBean() {
+        formato = FORMATO_24;
+        horaFinder = new Timer(100, this);
+        horaFinder.start();
     }
     
+    @Override
+    public void actionPerformed(ActionEvent e){
+        hora = LocalTime.now().getHour();
+        minuto = LocalTime.now().getMinute();
+        setText(getStringHora());
+    }
+    
+    private String getStringHora(){
+        int tHora = hora;
+        if(hora>12 && formato == FORMATO_12) tHora=hora-12;
+        return tHora+":"+minuto;
+    }
     
 }
